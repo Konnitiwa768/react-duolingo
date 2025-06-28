4c595de4d07273c76cf8f5826bc3233a38734781
Konnitiwa768 <konnitiwaa1@outlook.jp>
2025-06-28 15:26:33 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,17 +7,8 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  output: 'export', // 静的エクスポート有効
 
-  /**
-   * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
-   * out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
   images: {
     remotePatterns: [
       {
@@ -29,4 +20,5 @@ const config = {
     ],
   },
 };
+
 export default config;
